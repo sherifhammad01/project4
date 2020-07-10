797d52c7392453c494719ed27c02780ef86d8aad
EC2 Default User <ec2-user@ip-172-31-7-242.us-west-2.compute.internal>
2020-07-10 13:33:20 +0000

    push files

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=flaskskapp .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -it -p 8000:80 flaskskapp
--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,21 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=sherifhammad/flaskskapp
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+#kubectl run flaskskapp --image=$dockerpath --port=8080 --labels app=flaskskapp
+kubectl run flaskskapp\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=flaskskapp
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward flaskskapp 8000:80
